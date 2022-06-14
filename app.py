70dcea5b85203b0f33c84eb0b2b9d3a3bef979e3
Michael Law <michaellaw@gmx.com>
2022-06-13 20:34:49 -0400

    Code fix

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres//mlaw:cjiaang11@localhost/flask_starter_test_db'
 db = SQLAlchemy(app)
 
-app = Flask(__name__)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
 
 @app.route('/')
 def home():
     return {'message': 'Congratulations! Flask Starter is all set up!'}
-
